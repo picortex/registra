5a53054f84283c4ac860164e661c2dac4f25a5f7
andylamax <andylamax@programmer.net>
2022-11-18 20:44:31 +0300

    Added Daod implementations for registra

--- sdk/server/core/src/commonMain/kotlin/registra/RegistraInstaller.kt
+++ sdk/server/core/src/commonMain/kotlin/registra/RegistraInstaller.kt
@@ -3,6 +3,8 @@ package registra
 import bitframe.ApplicationConfigBuilder
 import bitframe.annotations.BitframeServerDsl
 import bitframe.mapService
+import bitframe.ControllerConfig
+import bitframe.ModuleConfig
 
 @BitframeServerDsl
 fun <S> ApplicationConfigBuilder<S>.installRegistra(builder: (S) -> RegistraServer) {
