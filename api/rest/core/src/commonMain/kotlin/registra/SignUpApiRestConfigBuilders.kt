5b1cac7e9bb01163699347267e3cd5374ce9bfe9
andylamax <andylamax@programmer.net>
2022-12-13 13:39:43 +0300

    Fixed default already bound

--- api/rest/core/src/commonMain/kotlin/registra/SignUpApiRestConfigBuilders.kt
+++ api/rest/core/src/commonMain/kotlin/registra/SignUpApiRestConfigBuilders.kt
@@ -11,18 +11,15 @@ import registra.internal.SignUpApiRestConfigImpl
 
 val SIGN_UP_API_REST_CONFIG_DEFAULT: SignUpApiRestConfig<Any, Any> = SignUpApiRestConfigImpl
 
-@PublishedApi
-internal val DEFAULT = SIGN_UP_API_REST_CONFIG_DEFAULT
-
 inline fun <E, H> SignUpApiRestConfig(
-    appId: String = DEFAULT.appId,
-    session: MutableLive<Session> = DEFAULT.session,
-    cache: Cache = DEFAULT.cache,
-    bus: EventBus = DEFAULT.bus,
-    logger: Logger = DEFAULT.logger,
-    executor: Executor = DEFAULT.executor,
+    appId: String = SIGN_UP_API_REST_CONFIG_DEFAULT.appId,
+    session: MutableLive<Session> = SIGN_UP_API_REST_CONFIG_DEFAULT.session,
+    cache: Cache = SIGN_UP_API_REST_CONFIG_DEFAULT.cache,
+    bus: EventBus = SIGN_UP_API_REST_CONFIG_DEFAULT.bus,
+    logger: Logger = SIGN_UP_API_REST_CONFIG_DEFAULT.logger,
+    executor: Executor = SIGN_UP_API_REST_CONFIG_DEFAULT.executor,
     endpoint: E,
-    codec: StringFormat = DEFAULT.codec,
+    codec: StringFormat = SIGN_UP_API_REST_CONFIG_DEFAULT.codec,
     http: H,
-    verificationUrl: String = DEFAULT.verificationUrl,
+    verificationUrl: String = SIGN_UP_API_REST_CONFIG_DEFAULT.verificationUrl,
 ): SignUpApiRestConfig<E, H> = SignUpApiRestConfigImpl(appId, session, cache, bus, logger, executor, endpoint, codec, http, verificationUrl)
--- api/rest/ktor/src/commonMain/kotlin/registra/SignUpApiRestKtorConfigConstructors.kt
+++ api/rest/ktor/src/commonMain/kotlin/registra/SignUpApiRestKtorConfigConstructors.kt
@@ -12,18 +12,15 @@ import registra.internal.SignUpApiRestKtorConfigImpl
 
 val SIGN_UP_API_REST_KTOR_CONFIG_DEFAULT: SignUpApiRestKtorConfig<Any> = SignUpApiRestKtorConfigImpl
 
-@PublishedApi
-internal val DEFAULT = SIGN_UP_API_REST_KTOR_CONFIG_DEFAULT
-
 inline fun <E> SignUpApiRestKtorConfig(
-    appId: String = DEFAULT.appId,
-    session: MutableLive<Session> = DEFAULT.session,
-    cache: Cache = DEFAULT.cache,
-    bus: EventBus = DEFAULT.bus,
-    logger: Logger = DEFAULT.logger,
-    executor: CoroutineExecutor = DEFAULT.executor,
+    appId: String = SIGN_UP_API_REST_KTOR_CONFIG_DEFAULT.appId,
+    session: MutableLive<Session> = SIGN_UP_API_REST_KTOR_CONFIG_DEFAULT.session,
+    cache: Cache = SIGN_UP_API_REST_KTOR_CONFIG_DEFAULT.cache,
+    bus: EventBus = SIGN_UP_API_REST_KTOR_CONFIG_DEFAULT.bus,
+    logger: Logger = SIGN_UP_API_REST_KTOR_CONFIG_DEFAULT.logger,
+    executor: CoroutineExecutor = SIGN_UP_API_REST_KTOR_CONFIG_DEFAULT.executor,
     endpoint: E,
-    codec: StringFormat = DEFAULT.codec,
-    http: HttpClient = DEFAULT.http,
-    verificationUrl: String = DEFAULT.verificationUrl,
+    codec: StringFormat = SIGN_UP_API_REST_KTOR_CONFIG_DEFAULT.codec,
+    http: HttpClient = SIGN_UP_API_REST_KTOR_CONFIG_DEFAULT.http,
+    verificationUrl: String = SIGN_UP_API_REST_KTOR_CONFIG_DEFAULT.verificationUrl,
 ): SignUpApiRestKtorConfig<E> = SignUpApiRestKtorConfigImpl(appId, session, cache, bus, logger, executor, endpoint, codec, http, verificationUrl)
