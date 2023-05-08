5dac11302692d360842d09699ad92a70b1612278
andylamax <andylamax@programmer.net>
2023-05-08 19:44:46 +0300

    Made paginator loader configurable even after initialization

--- sdk/client/core/src/commonMain/kotlin/registra/RegistraScopeConstructor.kt
+++ sdk/client/core/src/commonMain/kotlin/registra/RegistraScopeConstructor.kt
@@ -1,6 +1,5 @@
 package registra
 
 import registra.internal.RegistraScopeImpl
-import viewmodel.ScopeConfig
 
 inline fun RegistraScope(config: RegistraScopeConfig<RegistraApi>): RegistraScope = RegistraScopeImpl(config)
