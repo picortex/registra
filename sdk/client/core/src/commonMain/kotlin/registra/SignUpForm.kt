d4a259ae0ac9d383f3da93bf5baaa867e7b86450
andylamax <andylamax@programmer.net>
2023-06-18 02:15:24 +0300

    Finished the form refactor

--- sdk/client/core/src/commonMain/kotlin/registra/SignUpForm.kt
+++ sdk/client/core/src/commonMain/kotlin/registra/SignUpForm.kt
@@ -4,7 +4,7 @@
 package registra
 
 import symphony.Form
-import symphony.FormActionsBuildingBlock
+import symphony.FormInitialzer
 import symphony.FormConfig
 import kotlin.js.JsExport
 import registra.params.SignUpParams as Params
@@ -13,7 +13,7 @@ class SignUpForm(
     heading: String = "Create an account",
     details: String = "Signup in less than two minutes",
     config: FormConfig,
-    initializer: FormActionsBuildingBlock<Params, String>
+    initializer: FormInitialzer<Params, String>
 ) : Form<SignUpFields, Params, String>(
     heading = heading,
     details = details,
